e97294b6dce48af5b418c049f9cde2c2e8a2f48c
shreyanshi19 <shreya.arun19@rediffmail.com>
2016-09-07 12:26:12 +0530

    close to zero
    sum of three elements closest to zero

---
+++ I.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int m,a[10],i,j=1,k=2,close=0,ele1=0,ele2=0,e3=0;
+clrscr();
+scanf("%d",&m);
+for(i=0;i<m;i++)
+      scanf("%d",&a[i]);
+      close=a[i]+a[j]+a[k];
+for(i=0;i<m;i++)
+{
+for(j=i+1;j<m;j++)
+{
+for(k=j+1;k<m;k++)
+{
+if(abs(close)>abs(a[i]+a[j]+a[k]))
+      {  close=a[i]+a[j]+a[k];
+		ele1=a[i];
+		ele2=a[j];
+		e3=a[k];
+	}
+	 }
+	   } }  printf("%d%d%d",ele1,ele2,e3);
+
+	 getch();
+}
