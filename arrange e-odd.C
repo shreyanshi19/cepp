79e76f4c4ea2b10acddd148ff1bda20fb26f75a9
shreyanshi19 <shreya.arun19@rediffmail.com>
2016-09-07 12:22:13 +0530

    arrange even odd
    arrange even number at first than odd numbers

---
+++ arrange e-odd.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int m,a[10],i,c[10];
+clrscr();
+scanf("%d",&m);
+for(i=0;i<m;i++)
+      scanf("%d",&a[i]);
+
+for(i=0;i<m;i++)
+{
+if(a[i]%2==0)
+printf("%2d",a[i]);
+}
+for(i=0;i<m;i++)
+{
+ if(a[i]%2!=0)
+ printf("%2d",a[i]);
+ }
+	 getch();
+
+}
