04a2bdf4f5cc0915aa0c7488db67ff9e842a787d
shreyanshi19 <shreya.arun19@rediffmail.com>
2016-09-08 18:52:04 +0530

    closest to zero
    sum of 3 element  closeset to 0 in  a array.

--- I.C
+++ I.C
@@ -1,28 +1,30 @@
 #include<stdio.h>
-#include<conio.h>
 #include<math.h>
-void main()
+#define max 100
+int main()
 {
-int m,a[10],i,j=1,k=2,close=0,ele1=0,ele2=0,e3=0;
-clrscr();
-scanf("%d",&m);
-for(i=0;i<m;i++)
-      scanf("%d",&a[i]);
-      close=a[i]+a[j]+a[k];
-for(i=0;i<m;i++)
-{
-for(j=i+1;j<m;j++)
-{
-for(k=j+1;k<m;k++)
-{
-if(abs(close)>abs(a[i]+a[j]+a[k]))
-      {  close=a[i]+a[j]+a[k];
-		ele1=a[i];
-		ele2=a[j];
-		e3=a[k];
-	}
-	 }
-	   } }  printf("%d%d%d",ele1,ele2,e3);
-
-	 getch();
+	int m,a[max],i,j=1,k=2,close=0,ele1=0,ele2=0,ele3=0;
+	clrscr();
+	scanf("%d",&m);
+	for(i=0;i<m;i++)
+	 scanf("%d",&a[i]);
+      	close=a[i]+a[j]+a[k];
+	for(i=0;i<m;i++)
+	{
+		for(j=i+1;j<m;j++)
+		{
+			for(k=j+1;k<m;k++)
+			{
+				if(abs(close)>abs(a[i]+a[j]+a[k]))
+				 { 
+				 	close=a[i]+a[j]+a[k];
+					ele1=a[i];
+					ele2=a[j];
+					ele3=a[k];
+				}
+			 }
+		  }
+	}  
+	printf("%d%d%d",ele1,ele2,ele3);
+return 0;
 }
