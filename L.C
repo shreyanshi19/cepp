76253f1d1335e23ed726da7d4b8c2453ec5468a8
shreyanshi19 <shreya.arun19@rediffmail.com>
2016-09-08 18:26:11 +0530

    majority
    element  is majority  if it appears more than n/2 times in an array.

--- L.C
+++ L.C
@@ -1,38 +1,32 @@
 #include<stdio.h>
-#include<conio.h>
-
-void main()
+#define max 100
+int main()
 {
-int m,a[10],i,j,c[10]={0},e[10]={0},mum=0,flag=0;
-clrscr();
-scanf("%d",&m);
-for(i=0;i<m;i++)
-      scanf("%d",&a[i]);
-
-for(i=0;i<m;i++)
-{
-  for(j=i+1;j<m;j++)
-{
-if(a[i]==a[j])
-{
-c[i]++;
-e[i]=a[i];
-}
- }
-    }
- for(i=0;i<m;i++)
- {
-if(c[i]+1>m/2){
-	 mum=e[i];
-	 flag=1;
-	 }  }
+	int m,a[10],i,j,c[10]={0},e[10]={0},mum=0,flag=0;
+	clrscr();
+	scanf("%d",&m);
+	for(i=0;i<m;i++)
+      	scanf("%d",&a[i]);
+	for(i=0;i<m;i++)
+	{
+  		for(j=i+1;j<m;j++)
+		{
+			if(a[i]==a[j])
+			{
+				c[i]++;
+				e[i]=a[i];
+			}
+ 		}
+    	}
+ 	for(i=0;i<m;i++)
+ 	{
+		if(c[i]+1>m/2){
+		 mum=e[i];
+	 	flag=1;
+	 } 
 	 if(flag==1)
-	   printf("%d",mum);
+		 printf("%d",mum);
 		  else
-		   printf("ot");
-
-
-	 getch();
-
-
+		 printf("majority does not exist");
+return 0;
 }
