f001364690fcff9d6a07bcf1ac1d436093479e32
shreyanshi19 <shreya.arun19@rediffmail.com>
2016-09-08 18:33:24 +0530

    Maximum difference
    find maximum difference between two element such that larger element appears after smaller in  an array,

--- K.C
+++ K.C
@@ -1,28 +1,25 @@
 #include<stdio.h>
-#include<conio.h>
 #include<math.h>
-
-void main()
+#define max 100
+int main()
 {
-int m,a[10],i,j,mi=0,e=0,e2=0;
-clrscr();
-scanf("%d",&m);
-for(i=0;i<m;i++)
-      scanf("%d",&a[i]);
-
-for(i=0;i<m;i++)
-{
-  for(j=i+1;j<m;j++)
-{
-if(mi<abs(a[i]-a[j])&&(a[j]>a[i]))
-{
-mi=abs(a[i]-a[j]);
-e=a[i];
-e2=a[j];
+	int m,a[max],i,j,min=0,element1=0,element2=0;
+	clrscr();
+	scanf("%d",&m);
+	for(i=0;i<m;i++)
+      	scanf("%d",&a[i]);
+	for(i=0;i<m;i++)
+	{
+		 for(j=i+1;j<m;j++)
+		{
+			if(min<abs(a[i]-a[j])&&(a[j]>a[i]))
+			{
+				min=abs(a[i]-a[j]);
+				element1=a[i];
+				element2=a[j];
+			}
+		}
+	}
+	 printf("%d%2d",element1,element2);
+return 0;
 }
-}
-}
- printf("%d%2d",e,e2);
-
-	 getch();
-         }
